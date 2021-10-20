3b95c1191a1d640a405ea95aa030d1c7cbd31d9a
anpoliakov <Andrewlybimenko@gmail.com>
2021-10-20 22:39:28 +0300

    added thread pool

--- Multithreading/Multithreading/src/lesson_5/Test.java
+++ Multithreading/Multithreading/src/lesson_5/Test.java
@@ -2,13 +2,29 @@ package lesson_5;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+/*
+* Данные работники представленные пулом потоков - не будут хвататься за одну задачу
+* работа данных потоков из коробки уже отлична
+* */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //непосредственно сам ПУЛ ПОТОКОВ
         ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        //TODO описать и изучить 20.10
+        //даём задания для наших существующих 2-х потоков (2-м рабочим)
+        for(int i = 0; i < 5; i++){
+            //метод принимает класс реализующий Runnable с методом run()
+            executorService.submit(new Work(i));
+        }
+
+        //говорим сервису, что заданий больше нет и нужно приступать к выполнению заданий
+        executorService.shutdown(); //мгновенный выход из этого метода в поток main
+
+        //этот метод очень похож на метод join() только одно НО
+        //если после 1 часа потоки не выполнили свою работу - они завершаются принудительно
+        executorService.awaitTermination(1, TimeUnit.HOURS);
     }
 }
 
@@ -29,5 +45,7 @@ class Work implements Runnable{
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        System.out.println("Work " + id + " was completed");
     }
 }
