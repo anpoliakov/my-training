70ab784284737c3966d373894e171953025ae1f0
anpoliakov <Andrewlybimenko@gmail.com>
2021-10-23 21:28:27 +0300

    finished the pattern, producer-consumer

--- Multithreading/Multithreading/src/lesson_6/Main.java
+++ Multithreading/Multithreading/src/lesson_6/Main.java
@@ -11,25 +11,58 @@ public class Main {
     //просто синхронизированная очередь - для параллельной работы нескольких потоков
     private static BlockingQueue <Integer> queue = new ArrayBlockingQueue(10);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread threadOne = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    produce();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
+        Thread threadTwo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    consumer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        threadOne.start();
+        threadTwo.start();
+
+        //пишем, всёравно что наши второстепенные потоки никогда не завершаться
+        threadTwo.join();
+        threadOne.join();
     }
 
-    //представляет из себя - производителя
+    //представляет из себя - производителя (генерация данных в очередь)
     private static void produce() throws InterruptedException {
-        Random random = new Random(100);
+        Random random = new Random();
 
         while (true){
-            queue.put(random.nextInt());
+            /** СПЕЦ метод класса ArrayBlockingQueue, если очередь заполнена - ЖДЁТ пока появится место что бы добавить данные*/
+            queue.put(random.nextInt(100));
         }
     }
 
     //представляет из себя потребителя
     private static void consumer() throws InterruptedException {
+        Random random = new Random();
+
         while (true){
             Thread.sleep(100);
-            System.out.println(queue.take());
-            System.out.println("Size our queue is " + queue.size());
+            if(random.nextInt(10) == 5) {
+                /** СПЕЦ метод класса ArrayBlockingQueue, если очередь пуста - ЖДЁТ пока появится данные для считывания */
+                System.out.println(queue.take());
+                System.out.println("Size our queue is " + queue.size());
+            }
         }
     }
 }
