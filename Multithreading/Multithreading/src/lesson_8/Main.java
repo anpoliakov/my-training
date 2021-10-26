96079614b05f56ec5603a9655c89edbfdc611c18
anpoliakov <Andrewlybimenko@gmail.com>
2021-10-26 22:32:40 +0300

    working with class 'CountDownLatch'

---
+++ Multithreading/Multithreading/src/lesson_8/Main.java
@@ -0,0 +1,50 @@
+package lesson_8;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/*
+* Класс защёлка CountDownLatch - полностью синхронизированный,
+* для работы одновременно с множеством потоков. Отпирает место (места) в
+* потоке (в главнов или второстепенном), где был вызван метод countDownLatch.await().
+* При условии - если был вызван метод countDownLatch.countDown(); колличеством = числу
+* в параметрах при создании обьекта CountDownLatch.
+* */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+
+        //через пул потоков создами список заданий
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        for(int i = 0; i < 5; i++){
+            executorService.submit(new Processor(i,countDownLatch));
+        }
+        executorService.shutdown();
+
+        countDownLatch.await();
+        System.out.println("Программа выполнена, дождалась вызова 5 раз метода countDown()");
+    }
+}
+
+class Processor implements Runnable{
+    private int id;
+    private CountDownLatch cdl;
+
+    public Processor(int id, CountDownLatch cdl){
+        this.id = id;
+        this.cdl = cdl;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        cdl.countDown();
+        System.out.println("Thread - " + id);
+    }
+}
