c1a5b5eb2aaad7edd02c522dea5f47ac5d0cdb90
anpoliakov <Andrewlybimenko@gmail.com>
2021-10-31 19:49:26 +0300

    working class semaphore

--- Multithreading/Multithreading/src/lesson_10/Runner.java
+++ Multithreading/Multithreading/src/lesson_10/Runner.java
@@ -2,6 +2,7 @@ package lesson_10;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 /*
@@ -9,8 +10,6 @@ import java.util.concurrent.TimeUnit;
 * при создании экземпляра данного класса в параметрах указываем кол-во потоков которые могут
 * одновременно использовать этот один источник данных (в моём случае класс Connection)
 * */
-
-//ВАРИАНТ БЕЗ ИСПОЛЬЗОВАНИЯ Semaphore класса:
 public class Runner {
     public static void main(String[] args) throws InterruptedException {
         final int NUM_OF_THREAD = 200;
@@ -23,7 +22,7 @@ public class Runner {
                 @Override
                 public void run() {
                     try {
-                        connection.doWork();
+                        connection.work();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -39,6 +38,8 @@ public class Runner {
 /* singleton, образ моего подключения к серверу (показывает сколько сейчас подключено потоков
 которые совершают некоторую полезную работу */
 class Connection{
+    /* в параметрах указываем максимальное кол-во потокв - которые могут работают одновременно с ресурсом */
+    private Semaphore semaphore = new Semaphore(10);
     private static Connection connection = new Connection();
     private int connectionsCount;
 
@@ -48,7 +49,19 @@ class Connection{
         return connection;
     }
 
-    public void doWork() throws InterruptedException {
+    //одновременно смогут работать не более 10 потоков с данным методом
+    public void work() throws InterruptedException {
+        semaphore.acquire(); //берём одно разрешение
+        try {
+            doWork();
+        }finally {
+            // метод всегда прописываем в блоке finally (может быть ошибка в методе doWork()
+            // из за которой никогда не вернётся разрешение)
+            semaphore.release(); //возвращаем разрешение
+        }
+    }
+
+    private void doWork() throws InterruptedException {
         synchronized (this){
             connectionsCount++;
             System.out.println(connectionsCount);
