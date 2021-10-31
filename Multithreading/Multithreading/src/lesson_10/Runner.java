3ee65691da4269813dfff67f5ee5eb9a98b86d3d
anpoliakov <Andrewlybimenko@gmail.com>
2021-10-31 19:28:56 +0300

    Option without a class Semaphore

--- Multithreading/Multithreading/src/lesson_10/Runner.java
+++ Multithreading/Multithreading/src/lesson_10/Runner.java
@@ -1,17 +1,43 @@
 package lesson_10;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /*
 * Работа с классом Semaphore -  который регулирует работу потоков с одним источником данных
 * при создании экземпляра данного класса в параметрах указываем кол-во потоков которые могут
-* одновременно использовать этот один источник данных
+* одновременно использовать этот один источник данных (в моём случае класс Connection)
 * */
-public class Runner {
-    public static void main(String[] args) {
 
+//ВАРИАНТ БЕЗ ИСПОЛЬЗОВАНИЯ Semaphore класса:
+public class Runner {
+    public static void main(String[] args) throws InterruptedException {
+        final int NUM_OF_THREAD = 200;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(NUM_OF_THREAD);
+        Connection connection = Connection.getConnection();
+
+        for(int i = 0; i < NUM_OF_THREAD; i++){
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        connection.doWork();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.DAYS);
     }
 }
 
-//singleton класс - образ одного возможного подключения к серверу (который будет использоваться разными потоками)
+/* singleton, образ моего подключения к серверу (показывает сколько сейчас подключено потоков
+которые совершают некоторую полезную работу */
 class Connection{
     private static Connection connection = new Connection();
     private int connectionsCount;
@@ -22,6 +48,16 @@ class Connection{
         return connection;
     }
 
-    //TODO метод с полезной работой
+    public void doWork() throws InterruptedException {
+        synchronized (this){
+            connectionsCount++;
+            System.out.println(connectionsCount);
+        }
 
+        Thread.sleep(3000);
+
+        synchronized (this){
+            connectionsCount--;
+        }
+    }
 }
