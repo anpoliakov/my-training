a57417e7c051960642b80dc0d9e2378fce457aba
anpoliakov <Andrewlybimenko@gmail.com>
2021-11-04 22:20:43 +0300

    created a folder with tasks

---
+++ Multithreading/Multithreading/src/tasks/task1/MainTask1.java
@@ -0,0 +1,11 @@
+package tasks.task1;
+
+import java.util.ArrayList;
+
+public class MainTask1 {
+    private ArrayList <Integer> mas = new ArrayList<>();
+
+    public static void main(String[] args) {
+        MainTask1 main = new MainTask1();
+    }
+}
